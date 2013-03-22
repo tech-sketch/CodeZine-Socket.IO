5071b6c3f6058d75529fe1d280492a35ca95002a
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2013-03-22 18:40:32 +0900

    定義ファイルからPROTOCOLを削除（Java）

--- client/android/SocketIO-Client/src/jp/co/tis/stc/socketio/client/Common.java
+++ client/android/SocketIO-Client/src/jp/co/tis/stc/socketio/client/Common.java
@@ -3,8 +3,6 @@ package jp.co.tis.stc.socketio.client;
 public class Common {
     public static final String HOST = "xxxx.xxxx.dotcloud.com";
     public static final String PORT = "80";
-    public static final String PROTOCOL = "";
-//  public static final String PROTOCOL = "websocket";
     public static final String SEND_KEY = "message";
     public static final String RECV_KEY = "broadcast";
     
